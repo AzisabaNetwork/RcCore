5fc0a660ec9dae2ec186af3752ac1dfcedcc30ca
sysnote8main <sysnote8main@gmail.com>
2025-03-22 23:25:17 +0900

    feat: add Market restrict item system

--- src/main/java/net/azisaba/rccore/integration/auctionhouse/MarketEventListener.java
+++ src/main/java/net/azisaba/rccore/integration/auctionhouse/MarketEventListener.java
@@ -1,6 +1,7 @@
 package net.azisaba.rccore.integration.auctionhouse;
 
 import com.spawnchunk.auctionhouse.events.ListItemEvent;
+import net.azisaba.rccore.config.IntegrationConfig;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.event.EventHandler;
@@ -9,13 +10,20 @@ import org.bukkit.event.Listener;
 import java.util.List;
 
 public class MarketEventListener implements Listener {
-    // TODO: 販売禁止アイテムに関する実装
     @EventHandler
     public void onListNewItem(ListItemEvent event) {
         List<Component> loreList = event.getItem().lore();
-        if(loreList == null) return;
-        loreList.stream()
+        if (loreList == null) return;
+
+        boolean isDenyItem = loreList.stream()
                 .map(l -> LegacyComponentSerializer.legacyAmpersand().serialize(l))
-                .anyMatch(l -> l.contains(""));
+                .anyMatch(l ->
+                        IntegrationConfig.getMarketDenyItemLore().stream().anyMatch(l::contains)
+                );
+
+        if(isDenyItem) {
+            event.setCancelled(true);
+            // check: 何かしらのプレイヤーへのフィードバックがいるかも？
+        }
     }
 }
